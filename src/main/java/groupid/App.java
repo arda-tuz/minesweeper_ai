7b26d59d8d5eba7db8e8dc8ca743b5b8e9f0c433
ArdaTuz <verty.jump@gmail.com>
2024-02-04 00:23:58 +0300

    track edilmesi gereken tum dir'ler ilk kez add'lendi

---
+++ src/main/java/groupid/App.java
@@ -0,0 +1,10 @@
+package groupid;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println();
+        System.out.println( "minesweeper_ai" );
+    }
+}
